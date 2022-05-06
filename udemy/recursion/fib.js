aecfe2e30892476c77dd9ee5426362ca891723f0
ljsToha <ljstoha@naver.com>
2022-05-06 20:51:55 +0900

    udemy_singlyLinkedList
    udemy_singlyLinkedList

---
+++ udemy/recursion/fib.js
@@ -0,0 +1,13 @@
+// 피보나치 수열 1,1,2,3,5,8.......
+
+// 이해 안됨
+
+function fib(n) {
+  if (n <= 2) return 1;
+  return fib(n - 1) + fib(n - 2);
+}
+
+console.log(fib(4)); // 3
+console.log(fib(10)); // 55
+console.log(fib(28)); // 317811
+console.log(fib(35)); // 9227465
--- udemy/recursion/recursiveRange.js
+++ udemy/recursion/recursiveRange.js
@@ -1,1 +1,8 @@
 //0 부터 숫자의 합 구하기
+function recursiveRange(num) {
+  if (num === 0) return 0;
+  return num + recursiveRange(num - 1);
+}
+
+console.log(recursiveRange(6));
+console.log(recursiveRange(10));
